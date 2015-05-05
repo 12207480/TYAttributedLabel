227dce4578fbcda84ec4b6f95aef061b9a982fcb
sunyong0329 <233wangxiao@233wangxiaotekiiMac.local>
2015-05-05 16:27:01 +0800

    demo add more samples

--- TYAttributedLabelDemo/ViewController.m
+++ TYAttributedLabelDemo/ViewController.m
@@ -17,10 +17,27 @@
 #import "ParseTextViewController.h"
 #import "AddViewTextViewController.h"
 
+@interface tableViewItem : NSObject
+
+@property (nonatomic, strong) NSString *title;
+
+@property (nonatomic, strong) NSString *detailText;
+
+@property (nonatomic, assign) Class destVcClass;
+
+@end
+
+@implementation tableViewItem
+
+@end
+
 @interface ViewController ()<UITableViewDataSource,UITableViewDelegate>
 @property (nonatomic,strong) TYAttributedLabel *label;
-@property (nonatomic, strong) NSDictionary *dataDic;
+@property (nonatomic, weak) UITableView *tableView;
+
+@property (nonatomic, strong) NSMutableArray *itemArray;
 @end
+
 
 @implementation ViewController
 
@@ -29,10 +46,20 @@
     // Do any additional setup after loading the view, typically from a nib.
     self.edgesForExtendedLayout = UIRectEdgeNone;
     
-    self.dataDic = @{@"SimpleText":[SimpleTextViewController class],@"AttributedText":[AttributedTextViewController class],@"ImageText":[ImageTextViewController class],@"LinkText":[LinkTextViewController class],@"parseText":[ParseTextViewController class],@"AddViewText":[AddViewTextViewController class]};
-    
     [self addTableView];
     
+    [self addTableItems];
+    
+    [self.tableView reloadData];
+    
+}
+
+- (NSMutableArray *)itemArray
+{
+    if (_itemArray == nil) {
+        _itemArray = [NSMutableArray array];
+    }
+    return _itemArray;
 }
 
 - (void)addTableView
@@ -42,34 +69,66 @@
     tableView.delegate = self;
     tableView.dataSource = self;
     [self.view addSubview:tableView];
+    self.tableView = tableView;
+}
+
+- (void)addTableItems
+{
+    [self addTableItemWithTitle:@"SimpleText" detailText:@"简单文本显示" destVcClass:[SimpleTextViewController class]];
+    
+    [self addTableItemWithTitle:@"AttributedText" detailText:@"属性文本显示" destVcClass:[AttributedTextViewController class]];
+    
+    [self addTableItemWithTitle:@"LinkText" detailText:@"属性链接文本显示" destVcClass:[LinkTextViewController class]];
+    
+    [self addTableItemWithTitle:@"ImageText" detailText:@"属性文本和Image混排显示" destVcClass:[ImageTextViewController class]];
+    
+    [self addTableItemWithTitle:@"AddViewText" detailText:@"属性文本和UIView混排显示" destVcClass:[AddViewTextViewController class]];
+    
+    [self addTableItemWithTitle:@"parseText" detailText:@"自定义排版解析图文混排显示" destVcClass:[ParseTextViewController class]];
+}
+
+- (void)addTableItemWithTitle:(NSString *)title detailText:(NSString *)detailText destVcClass:(Class)destVcClass
+{
+    tableViewItem *item = [[tableViewItem alloc]init];
+    item.title = title;
+    item.detailText = detailText;
+    item.destVcClass = destVcClass;
+    
+    [self.itemArray addObject:item];
 }
 
 #pragma mark - UITableViewDataSource
 
 - (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
 {
-    return self.dataDic.count;
+    return self.itemArray.count;
 }
 
 - (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
 {
     UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"cell"];
     if (cell == nil) {
-        cell = [[UITableViewCell alloc]initWithStyle:UITableViewCellStyleDefault reuseIdentifier:@"cell"];
+        cell = [[UITableViewCell alloc]initWithStyle:UITableViewCellStyleSubtitle reuseIdentifier:@"cell"];
         cell.accessoryType = UITableViewCellAccessoryDisclosureIndicator;
     }
     
-    cell.textLabel.text = [self.dataDic allKeys][indexPath.row];
+    tableViewItem *item = self.itemArray[indexPath.row];
+    cell.textLabel.text = item.title;
+    cell.detailTextLabel.text = item.detailText;
+    
     return cell;
 }
 
 - (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath
 {
-    Class class = [self.dataDic allValues][indexPath.row];
-    UIViewController *VC = [[class alloc]init];
-    VC.view.backgroundColor = [UIColor whiteColor];
-    VC.title = [self.dataDic allKeys][indexPath.row];
-    [self.navigationController pushViewController:VC animated:YES];
+    tableViewItem *item = self.itemArray[indexPath.row];
+    
+    if (item.destVcClass ) {
+        UIViewController *vc = [[item.destVcClass alloc]init];
+        vc.view.backgroundColor = [UIColor whiteColor];
+        vc.title = item.title;
+        [self.navigationController pushViewController:vc animated:YES];
+    }
 }
 
 - (void)didReceiveMemoryWarning {
