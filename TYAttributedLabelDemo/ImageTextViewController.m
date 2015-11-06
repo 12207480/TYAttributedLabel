390957aa12a566675e634cf484671ed11cbf0d03
tany <122074809@qq.com>
2015-11-06 13:48:57 +0800

    v2.5 code refactor,move label some operation to textContainer

--- TYAttributedLabelDemo/ImageTextViewController.m
+++ TYAttributedLabelDemo/ImageTextViewController.m
@@ -24,8 +24,6 @@
     [super viewDidLoad];
     // Do any additional setup after loading the view.
     
-    self.navigationItem.rightBarButtonItem = [[UIBarButtonItem alloc]initWithTitle:@"刷新" style:UIBarButtonItemStyleDone target:self action:@selector(updateLabel)];
-    
     [self addScrollView];
     
     // appendAttributedText
@@ -166,19 +164,6 @@
     _label2 = label;
     
     [_scrollView setContentSize:CGSizeMake(0, CGRectGetMaxY(label.frame)+10)];
-}
-
-- (void)updateLabel
-{
-//    [_label2 updateLayoutWithEnumerateTextStorageUsingBlock:^(id<TYTextStorageProtocol> textStorage, NSUInteger idx, BOOL *stop) {
-//        if ([textStorage isKindOfClass:[TYImageStorage class]]) {
-//            TYImageStorage *imageStorage = textStorage;
-//            
-//            if (imageStorage.size.width > 100) {
-//                imageStorage.size = CGSizeMake(imageStorage.size.width/2, imageStorage.size.height/2);
-//            }
-//        }
-//    }];
 }
 
 #pragma mark - deleagte
